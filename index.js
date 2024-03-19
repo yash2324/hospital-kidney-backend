438d8471bef32988ab7f21cb6fdbc5a5ca7f1dd1
Yash <yg232004@gmail.com>
2024-03-20 03:55:57 +0530

    added put and delete

--- index.js
+++ index.js
@@ -35,3 +35,21 @@ app.post("/", (req, res) => {
     msg: "done",
   });
 });
+app.put("/", (req, res) => {
+  for (let i = 0; i < users[0].kidneys.length; i++) {
+    users[0].kidneys[i].healthy = true;
+  }
+  res.json();
+});
+app.delete("/", (req, res) => {
+  const newKidneys = [];
+  for (let i = 0; i < users[0].kidneys.length; i++) {
+    if (users[0].kidneys[i].healthy) {
+      newKidneys.push({
+        healthy: true,
+      });
+    }
+  }
+  users[0].kidneys = newKidneys;
+  res.json({ msg: "done" });
+});
